0c4e6650db8b9c4098ee8c358c2b3f089a6858e0
drianodev <adrianomendes661@gmail.com>
2024-01-03 17:06:08 -0300

    initial commit

---
+++ src/test/java/br/com/drianodev/cachespring/CacheSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.drianodev.cachespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
